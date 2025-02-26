bfad3400bf54fb29637ee9aa6002d6e206e6a12d
Carlos Guimaraes <sekmet@open4g.com>
2025-02-26 07:48:57 -0300

    fix: onboarding small fix

--- app/src/routes/_authenticated/route.tsx
+++ app/src/routes/_authenticated/route.tsx
@@ -20,11 +20,31 @@ function RouteComponent() {
     router.navigate({ to: '/onboarding' })
   }
 
-  return (
+  return onboarding ? (
     <SearchProvider>
       <SidebarProvider defaultOpen={defaultOpen}>
         <SkipToMain />
-        {onboarding ? null : (<AppSidebar />)}
+        <div
+          id='content'
+          className={cn(
+            'ml-auto w-full max-w-full',
+            'peer-data-[state=collapsed]:w-[calc(100%-var(--sidebar-width-icon)-1rem)]',
+            'peer-data-[state=expanded]:w-[calc(100%-var(--sidebar-width))]',
+            'transition-[width] duration-200 ease-linear',
+            'flex h-svh flex-col',
+            'group-data-[scroll-locked=1]/body:h-full',
+            'group-data-[scroll-locked=1]/body:has-[main.fixed-main]:h-svh'
+          )}
+        >
+          <Outlet />
+        </div>
+      </SidebarProvider>
+    </SearchProvider>
+  ): (
+    <SearchProvider>
+      <SidebarProvider defaultOpen={defaultOpen}>
+        <SkipToMain />
+        <AppSidebar />
         <div
           id='content'
           className={cn(
