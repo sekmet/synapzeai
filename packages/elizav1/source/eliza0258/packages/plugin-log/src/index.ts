fe5497a6a20dd2d7ffd33ed59ec700ae6ade6c7a
Carlos Guimaraes <sekmet@open4g.com>
2025-03-01 08:35:26 -0300

    fix: agent setup fix

--- packages/elizav1/source/eliza0258/packages/plugin-log/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { logPlugin } from './plugins/log-plugin';
-import { LogService } from './services/log.service';
-
-export * from './plugins/log-plugin';
-export { LogService };
-
-export default logPlugin;
